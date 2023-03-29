c5ed16bd9511d2d9703368422286cf4c59b0aa33
waldmanido2000 <112062207+waldmanido2000@users.noreply.github.com>
2023-03-29 14:35:27 +0300

    multiple videos

--- extract.js
+++ extract.js
@@ -1,16 +1,18 @@
 javascript:(function() {
-  var videoElement = document.querySelector('video');
-  if (videoElement) {
-    var videoURL = videoElement.src;
-    var originalFilename = videoElement.getAttribute('data-filename') || 'video.mp4';
-    var link = document.createElement('a');
-    link.href = videoURL;
-    link.download = originalFilename;
-    link.style.display = 'none';
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
+  var videoElements = document.querySelectorAll('video');
+  if (videoElements.length > 0) {
+    videoElements.forEach(function(videoElement, index) {
+      var videoURL = videoElement.src;
+      var originalFilename = videoElement.getAttribute('data-filename') || 'video_' + (index + 1) + '.mp4';
+      var link = document.createElement('a');
+      link.href = videoURL;
+      link.download = originalFilename;
+      link.style.display = 'none';
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+    });
   } else {
-    alert('No video found on the page.');
+    alert('No videos found on the page.');
   }
 })();
