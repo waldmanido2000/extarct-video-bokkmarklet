9d4349dbfe66783397ce3ad92c3815d02bd2169f
waldmanido2000 <112062207+waldmanido2000@users.noreply.github.com>
2023-04-03 17:08:51 +0300

    add quantity selector to word count

--- wordCount.js
+++ wordCount.js
@@ -19,12 +19,17 @@ javascript:(function() {
       return wordCounts;
     }
   
-    function exportToCSV(wordList) {
+    function sortWordCounts(wordCounts) {
+      return Object.entries(wordCounts).sort((a, b) => b[1] - a[1]);
+    }
+  
+    function exportToCSV(sortedWordList, numWords) {
       const currentDate = new Date().toLocaleString();
       const pageURL = document.URL;
       const header = `Date and Time: ${currentDate}\nWebpage URL: ${pageURL}\n\nWord,Count\n`;
       const bom = '\ufeff';
-      const csvContent = 'data:text/csv;charset=utf-8,' + bom + header + Object.entries(wordList).map(([word, count]) => `${word},${count}`).join('\n');
+      const topWords = sortedWordList.slice(0, numWords);
+      const csvContent = 'data:text/csv;charset=utf-8,' + bom + header + topWords.map(([word, count]) => `${word},${count}`).join('\n');
       const encodedUri = encodeURI(csvContent);
       const link = document.createElement('a');
       link.setAttribute('href', encodedUri);
@@ -35,6 +40,12 @@ javascript:(function() {
     }
   
     const wordList = extractWords();
-    exportToCSV(wordList);
+    const sortedWordList = sortWordCounts(wordList);
+    const numWords = parseInt(prompt('Enter the number of words to export:', '20'));
+    if (!isNaN(numWords) && numWords > 0) {
+      exportToCSV(sortedWordList, numWords);
+    } else {
+      alert('Invalid input. Please enter a positive number.');
+    }
   })();
   
