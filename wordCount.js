493c469048133d334a98a3b1c926322ae799defd
waldmanido2000 <112062207+waldmanido2000@users.noreply.github.com>
2023-04-03 16:25:38 +0300

    fixed word count encoding

--- wordCount.js
+++ wordCount.js
@@ -2,9 +2,8 @@ javascript:(function() {
     function extractWords() {
       const bodyText = document.body.innerText;
       const words = bodyText
-        .replace(/[^\w\s]|_/g, '')
+        .replace(/[^\u0590-\u05FF\w\s]|_/g, '')
         .replace(/\s+/g, ' ')
-        .toLowerCase()
         .split(' ');
   
       const wordCounts = {};
@@ -20,7 +19,22 @@ javascript:(function() {
       return wordCounts;
     }
   
+    function exportToCSV(wordList) {
+      const currentDate = new Date().toLocaleString();
+      const pageURL = document.URL;
+      const header = `Date and Time: ${currentDate}\nWebpage URL: ${pageURL}\n\nWord,Count\n`;
+      const bom = '\ufeff';
+      const csvContent = 'data:text/csv;charset=utf-8,' + bom + header + Object.entries(wordList).map(([word, count]) => `${word},${count}`).join('\n');
+      const encodedUri = encodeURI(csvContent);
+      const link = document.createElement('a');
+      link.setAttribute('href', encodedUri);
+      link.setAttribute('download', 'word_counts.csv');
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+    }
+  
     const wordList = extractWords();
-    console.table(wordList);
+    exportToCSV(wordList);
   })();
   
